d7c99c10318c6304a51e5e74fbab72f26c4b013c
!MikaSukie(ミカスキエ)! <75727770+MikaSukie@users.noreply.github.com>
2025-02-17 22:16:49 +0100

    Delete Json-IDizer.bat
    Not needed anymore

--- Json-IDizer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\HandieMC\PycharmProjects\CUNE-VEngine\"  # Change to the directory containing your script
-python3 Json-ID-Assigner.py  # Replace 'script_name.py' with your script's filename
-pause  # Keeps the terminal open after execution (optional)
